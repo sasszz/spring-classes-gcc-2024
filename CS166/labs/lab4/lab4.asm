b540b4ea06be04fd152a3da7f889e8d5bbf7b4fe
sasszz <luchevreuil@gmail.com>
2024-06-07 10:51:48 -0700

    lab4 166

--- CS166/labs/lab4/lab4.asm
+++ CS166/labs/lab4/lab4.asm
@@ -1,46 +1,107 @@
 / Lucie Chevreuil
 / Lab 4, CS 166
+      *0100
+	    jmp main
+msg, "Sorted\n\0"
+/ print the global array
+p= 0
+x= 2
+prtary, enter 3
+        cam
+        tadam aarray
+        dcam s p        / set up pointer
+        dca s x         / set up counter
+loop,   tad is p        / print number
+        jms i adecout
+        tad nl          / print newline
+        tls
+        cla
+        isz s p
+        isz s x
+        tad s x
+        cma iac
+        tad size
+        sza cla
+        jmp loop
+        ret 3            / remove 3 local words
+adecout, decout
+nl,     '\n'
 
-          *0100
-          jmp main
-numbers,  12; 6; 38; 34; 5; 29; 17
-size,     7
-anumbers, numbers
-main,     cam
-          tadam anumbers
-          enter 2
-          dcam s tos / put on top of stack
-          jms print
-          leave 2
-          hlt
+array, 	12; 44; 3; 25; 67; 59; 8
+size,   7
+aarray, array
 
-/ print the array
-index= 0            / for loop index
-ary= 3              / parm pointer to array
-print,    cla       / set up index
-          push
-for,      tad s index       / check index < size
-          cma iac           / make negative
-          tad size
-          sna cla           / exit?
-          jmp forexit       / yes!
-          tad is ary        / get the value
-          jms decout        / print
-          tad nl
-          tls               / print newline
-          isz s index       / increment index
-          cla
-          isz s ary
-          jmp for
-forexit,  ret 1               / remove index and return
-nl,       '\n'
-tos=      0 / top of stack
-prt_msg,  pusham   / save the message address and clear AC/MQ
+main,   jms prtary       / print the array
+        tadam aarray     / get the address
+        pusham           / push as a parm
+        jms bsort
+        popma            / remove parm
+        cam
+        tadam amsg
+        jms i aprt_msg
+        jms prtary       / print the array
+        hlt
+amsg, msg
+aprt_msg, prt_msg
+                         / bubble sort
+                         /   pass it the address of the array
+                         /   use global size for size
+/ locals
+ptr0= 0                  / pointer with 0, 1
+ptr1= 2                  / pointer with 2, 3
+index= 4
+swapped= 5
+temp= 6
+pass= 7
+                         / return 8, 9
+                         / parm, ptr to ary
+ary=  10                 / also take 11
+SWAP= 07403	
+bsort,  enter 8          / allocate ptrs and locals
+        cla iac
+        dca s pass       / pass = 1
+do,     cam
+        tadam s ary
+        dcam s ptr0      / point to ary[index]
+        tadam s ary
+        dcam s ptr1
+        isz s ptr1       / point to ary[index+1]
+        cla              / set swapped to false
+        dca s swapped
+        dca s index      / set index to 0
+for,    tad s index      / get index
+        tad s pass       / make negative
+        cma iac
+        tad size         / index + pass < size?
+        sna cla     
+        jmp endfor       / no
+        tad is ptr0      / get ary[index]
+        cma iac          / make negative
+        tad is ptr1      / ary[i] > ary[i + 1]?
+        sma cla     
+        jmp bump         / no
+        cam
+        tadam s ptr0
+        SWAP
+        iac             / swapped = true
+        dca s swapped
+bump,   isz s index     / index++
+        isz s ptr0 
+        isz s ptr1
+        jmp for
+endfor, isz s pass      / pass++
+        tad s swapped   / swapped = true?
+        sza cla
+        jmp do          / yes
+        ret 8           / remove locals and return
+        
+tos=      0             / top of stack
+prt_msg,  pusham        / save the message address and clear AC/MQ
 prt_loop, tad is tos
           sna
-          ret 2       / null character, exit
-          tpc         / else print the character
-          isz s tos  / update pointer
+          ret 2         / null character, exit
+          tpc           / else print the character
+          isz s tos     / update pointer
           cla
           jmp prt_loop
 
@@ -86,8 +147,11 @@ dec_minus, -'-'
 dec_negnl, -'\n'
 decin,    enter 2       / make space for minus flag, and digit
 	  dca s dec_flag  / clear flag
-          mql         / clear multipler quotient
+dec_ws,   cam         / clear ac and multipler quotient
           krb          / read first digit
+	  jms dec_chkws / see if it is ws
+	  spa 
+          jmp dec_ws    / yes, ws, skip by getting next char
           dca s dec_digit / save it
           tad dec_minus / minus sign?
           tad s dec_digit
@@ -97,9 +161,9 @@ decin,    enter 2       / make space for minus flag, and digit
           dca s dec_flag
           tad dec_asc0
           dca s dec_digit / replace minus with leading zero
-deci_lp,  tad dec_negnl / newline?
-          tad s dec_digit
-          sza cla
+deci_lp,  tad s dec_digit / ws?
+	  jms dec_chkws
+          sma cla
           jmp deci_cont / no, add digit
           tad s dec_flag   / exit, but should negate number?
           sna cla    
@@ -118,7 +182,7 @@ deci_cont, tad s dec_digit  / get ascii digit
           tad s dec_digit
           spa cla 
           jmp deci_m10 / yes, add to number
-          tad dec_errin / no, print error and leave
+          tad adec_errin / no, print error and leave
           jms i iprtmsg  / print messag
           jmp deci_ret 
 deci_m10, muy  / multiply by 10
@@ -129,6 +193,26 @@ deci_m10, muy  / multiply by 10
           krb         / get next digit
           dca s dec_digit
           jmp deci_lp
-
+/ Checks to see if AC has a white space char (ws)
+/ if so, it returns with
+dec_chkws, push       / save character
+          tad s tos   / get it back
+          tad dec_negsp  / space?
+	  sna cla 
+          jmp dec_chkx / yes, signal back
+	  tad s tos   / is it a nl?
+          tad dec_negnl 
+          sna cla
+          jmp dec_chkx / yes, signal back
+          tad s tos    / is it a tab?
+	  tad dec_negtab
+          sza cla
+          jmp dec_chkok / no, char is not ws
+dec_chkx,  cla cll cml rar / put a 1 in upper bit
+dec_chkok, tad s tos   / put char back into ac
+	   ret 1      / remove char before returning
+dec_negsp,   -' '      / negative space
+dec_negtab, -'\t'    / negative tab
+adec_errin, dec_errin
 dec_errin, "Bad digit\n\0"
 
